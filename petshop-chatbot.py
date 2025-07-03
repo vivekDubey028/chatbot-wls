8009491cc20290b8dc90c16ecefbb84e4565fdf9
vivekDubey028 <147794342+vivekDubey028@users.noreply.github.com>
2025-07-03 12:37:22 +0530

    fifth commit

--- petshop-chatbot.py
+++ petshop-chatbot.py
@@ -234,4 +234,5 @@ def serve_favicon():
     return "", 204  # No content
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
